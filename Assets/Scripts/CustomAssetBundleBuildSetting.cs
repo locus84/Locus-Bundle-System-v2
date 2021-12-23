d448c0e88e6946f48a379952f7efaadd005bfb84
locus <locus84@gmail.com>
2021-12-24 03:40:08 +0900

    set remote url functionality, better build stretegy, remove warnning

--- Assets/Scripts/CustomAssetBundleBuildSetting.cs
+++ Assets/Scripts/CustomAssetBundleBuildSetting.cs
@@ -11,9 +11,9 @@ public class CustomAssetBundleBuildSetting : AssetBundleBuildSetting
         var bundleSettings = new List<BundleSetting>();
 
         AddFilesInFolder("Local", "Assets/TestRemoteResources/Local", true, true, true, bundleSettings);
-        AddFilesInFolder("Object", "Assets/TestRemoteResources/Object", true, true, false, bundleSettings);
-        AddFilesInFolder("Object_RootOnly", "Assets/TestRemoteResources/Object_RootOnly", true, false, true, bundleSettings);
-        AddFilesInFolder("Scene", "Assets/TestRemoteResources/Scene", true, true, false, bundleSettings);
+        AddFilesInFolder("Object", "Assets/TestRemoteResources/Object", false, true, true, bundleSettings);
+        AddFilesInFolder("Object_RootOnly", "Assets/TestRemoteResources/Object_RootOnly", false, false, true, bundleSettings);
+        AddFilesInFolder("Scene", "Assets/TestRemoteResources/Scene", false, true, true, bundleSettings);
 
         return bundleSettings;
     }
